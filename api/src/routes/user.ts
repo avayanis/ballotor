cfe11686fe6c6134f1ed0a3fcd01cab0df21af0f
Andranik Tonoyan <xdranik@gmail.com>
2018-04-28 21:55:28 -0700

    Create user logic

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,12 +1,27 @@
-import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+import * as config from "config";
+import * as request from "request-promise";
 import { verifyJWT } from "../helpers/validators";
+import { createUser } from "../controllers/user";
+import logger from "../helpers/logger";
+
+// types
+import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+import { IncomingMessage, ServerResponse } from "http";
 
 export default async function routes(server: FastifyInstance, options: any) {
-  server.get(
+  server.post(
     "/user",
-    { beforeHandler: verifyJWT },
-    async (request: FastifyRequest<{}>, reply: FastifyReply<{}>) => {
-      return { hello: "world" };
+    async (
+      request: FastifyRequest<IncomingMessage>,
+      reply: FastifyReply<ServerResponse>
+    ) => {
+      try {
+        const user = await createUser(request.body);
+        return { success: true };
+      } catch (err) {
+        logger.warn(err);
+        return { success: false };
+      }
     }
   );
 }
