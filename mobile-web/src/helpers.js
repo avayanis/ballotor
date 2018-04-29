3a93c03b54a66c864affbd095b67f3959da3e406
Aaron Hathaway <amhathaw@gmail.com>
2018-04-29 01:36:19 -0700

    It's working :D

--- mobile-web/src/helpers.js
+++ mobile-web/src/helpers.js
@@ -1,21 +1,20 @@
-/*
- * Copyright (c) 2018, Okta, Inc. and/or its affiliates. All rights reserved.
- * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
- *
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *
- * See the License for the specific language governing permissions and limitations under the License.
- */
+import * as OktaSignIn from "@okta/okta-signin-widget";
 
-/**
- * Helper function that watches the authenticate state, then applies it
- * as a boolean (authenticated) as well as attaches the userinfo data.
- */
-async function checkAuthentication() {
+import config from "./.config";
+
+const kIdToken = "id_token";
+const kAccessToken = "access_token";
+
+export async function checkAuthentication() {
+  console.log("heeeeere");
+  await parseTokensFromUrlIfNecessary();
+  getTokensFromLocalStorage();
+  const userinfo = await this.props.auth.getUser();
+  console.log("userinfo", userinfo);
+
   const authenticated = await this.props.auth.isAuthenticated();
+  console.log("authenticated", authenticated);
+
   if (authenticated !== this.state.authenticated) {
     if (authenticated && !this.state.userinfo) {
       const userinfo = await this.props.auth.getUser();
@@ -26,5 +25,87 @@ async function checkAuthentication() {
   }
 }
 
-/* eslint-disable import/prefer-default-export */
-export { checkAuthentication };
+async function parseTokensFromUrlIfNecessary() {
+  return new Promise(resolve => {
+    let signIn = getSignIn();
+    if (!signIn || !signIn.token || !signIn.token.hasTokensInUrl()) {
+      resolve(false);
+    }
+
+    signIn.token.parseTokensFromUrl(
+      async function success(res) {
+        //console.log("debug", res[0], res[1]);
+        const idToken = res[0];
+        const accessToken = res[1];
+        signIn.tokenManager.add(kIdToken, idToken);
+        signIn.tokenManager.add(kAccessToken, accessToken);
+        localStorage.setItem(
+          "okta-token-storage",
+          JSON.stringify({ idToken: idToken, accessToken: accessToken })
+        );
+        setObjectInLocalStorage(kIdToken, idToken);
+        setObjectInLocalStorage(kAccessToken, accessToken);
+        resolve(true);
+      },
+      async function error(err) {
+        // We're okay with there being an error
+        resolve(false);
+      }
+    );
+  });
+}
+
+function getTokensFromLocalStorage() {
+  console.log("and here");
+  const idToken = getObjectFromLocalStorage(kIdToken);
+  console.log("but not here");
+  console.log("idToken", idToken);
+  if (idToken) {
+    signIn.tokenManager.add(kIdToken, idToken);
+    console.log("~~~~~");
+  }
+
+  const accessToken = getObjectFromLocalStorage(kAccessToken);
+  console.log("accessToken", accessToken);
+  if (accessToken) {
+    console.log("accessToken", accessToken);
+    signIn.tokenManager.add(kAccessToken, accessToken);
+  }
+  signIn.tokenManager.refresh();
+}
+
+function getObjectFromLocalStorage(key) {
+  const json = localStorage.getItem(key);
+  try {
+    return JSON.parse(json);
+  } catch (err) {}
+  return null;
+}
+
+function setObjectInLocalStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+let signIn = null;
+export function getSignIn() {
+  if (signIn === null) {
+    signIn = new OktaSignIn({
+      baseUrl: config.oidc.issuer.split("/oauth2")[0],
+      clientId: config.oidc.clientId,
+      redirectUri: config.oidc.redirectUri,
+      logo: "/react.svg",
+      i18n: {
+        en: {
+          "primaryauth.title": "Sign in to React & Company"
+        }
+      },
+      authParams: {
+        responseType: ["id_token", "token"],
+        issuer: config.oidc.issuer,
+        display: "page",
+        scopes: config.oidc.scope.split(" ")
+      }
+    });
+  }
+  return signIn;
+}
