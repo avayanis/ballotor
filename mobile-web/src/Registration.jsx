4db02dfbfe26506f7af2f38732a113a17e12cf5a
Aaron Hathaway <amhathaw@gmail.com>
2018-04-29 01:56:23 -0700

    Get rid of post-install.js and change registration's config

--- mobile-web/src/Registration.jsx
+++ mobile-web/src/Registration.jsx
@@ -1,21 +1,9 @@
-/*
- * Copyright (c) 2018, Okta, Inc. and/or its affiliates. All rights reserved.
- * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
- *
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *
- * See the License for the specific language governing permissions and limitations under the License.
- */
-
 import React, { Component } from "react";
 import { Container, Icon, Image, Menu } from "semantic-ui-react";
 import { Card, CardTitle, TextField, RaisedButton } from "material-ui";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import $ from "jquery";
-import config from "./.samples.config";
+import config from "./.config";
 import "./Registration.css";
 
 export default class Registration extends Component {
